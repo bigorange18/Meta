a02de7b62cf514476df806931530ce10e56908bc
orange <chenorange2219@gmail.com>
2023-10-22 17:11:07 +0800

    添加检测代码

---
+++ src/detect.py
@@ -0,0 +1,37 @@
+import os, sys
+import torch
+from pathlib import Path
+from lib.genral import increment_path, select_device
+
+FILE = Path(__file__).resolve()         # 转为绝对路径
+ROOT = FILE.parents[0]                  # 找到路径
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))          # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+def detect(
+        source = "0",
+        savefile = ROOT / "runs/detect",
+        name = "exp",
+        device=''
+
+):
+    '''
+    1、判断输入的是视频还是开启摄像头
+    '''
+    source = str(source)
+    is_cam = source.isnumeric() or source.endswith(".stream")
+    # 传入视频是以screen开头
+    is_video = source.lower().startswith("screen")
+    # 创建新的文件夹保存输出结果
+    save_file = increment_path(Path(save_file) / name, exist_ok=False)
+
+    #gpu
+    # device = torch.device("device:0")
+    device = select_device(device)
+
+
+
+
+
+print(ROOT)
