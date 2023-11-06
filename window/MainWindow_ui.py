fe3aa39798ba31cf179013685286a15a781730eb
orange <chenorange2219@gmail.com>
2023-11-06 08:08:07 +0800

    update

---
+++ window/MainWindow_ui.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'MainWindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QSizePolicy, QTabWidget,
+    QVBoxLayout, QWidget)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(1202, 568)
+        self.verticalLayout = QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.tabWidget = QTabWidget(Dialog)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.verticalLayout.addWidget(self.tabWidget)
+
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Dialog", u"Tab 1", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Dialog", u"Tab 2", None))
+    # retranslateUi
+
--- window/test.py
+++ window/test.py
@@ -6,26 +6,56 @@ import sys
 
 # 任何一个PySide界面程序都需要使用QApplication
 # 我们要展示一个普通的窗口，所以需要导入QWidget，用来让我们自己的类继承
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget,QMessageBox
+from PySide6.QtWidgets import QMainWindow, QDialog, QLabel, QPushButton, QLineEdit
+from PySide6.QtGui import QCursor
+from PySide6.QtCore import Qt
 # 导入我们生成的界面
 from untitled_ui import Ui_Dialog
  
+import qtmodern.styles
+from qtmodern import styles, windows
+
 # 继承QWidget类，以获取其属性和方法
-class MyWidget(QWidget):
-    def __init__(self):
+class MainWindow(QWidget, Ui_Dialog):
+    def __init__(self, win_width=500):
         super().__init__()
+        # 设置窗口大小
+        self.setFixedSize(win_width, int(win_width*0.75))
+        self.move(1000, 200)
+        # 
+        # self.setStyleSheet("background: rgb(132, 189, 179);")
+
         # 设置界面为我们生成的界面
-        self.ui = Ui_Dialog()
-        self.ui.setupUi(self)
+        self.setupUi(self)
+
+        # 给登录按钮增加属性
+        self.pushButton.setCursor(QCursor(Qt.PointingHandCursor))
+        self.pushButton.setStyleSheet(
+            "*{border: 4px solid '#BC006C;" +
+            "border-radius: 45px;" +
+            "font-size: 35px;" + 
+            "color: 'white';" + 
+            "padding: 25px 0;" + 
+            "margin: 100px 200px;}" +
+            "*:hover{background: '#BC006C'};")
+        # self.pushButton.clicked.connect(self.on_botton_clicked)
+        # self.show()
+
+    def on_botton_clicked(self):
+        msg = QMessageBox.clickedButton(QMessageBox(), "提示确定")
+        print("button clicked")
 
 # 程序入口
 if __name__ == "__main__":
     # 初始化QApplication，界面展示要包含在QApplication初始化之后，结束之前
     app = QApplication(sys.argv)
- 
-    # 初始化并展示我们的界面组件
-    window = MyWidget()
-    window.show()
     
+    # qtmodern.styles.dark(app)
+    # 初始化并展示我们的界面组件
+    window = MainWindow()
+    # window.show()
+    # win = qtmodern.windows.ModernWindow(window)
+    window.show() 
     # 结束QApplication
     sys.exit(app.exec_())
